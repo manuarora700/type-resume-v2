c031be990ed0be2911104610ef1b6d46424da388
Manu Arora <mannuarora700@gmail.com>
2020-03-02 22:03:41 +0530

    Init // Final // Updated

---
+++ README.md
@@ -0,0 +1,21 @@
+# Type Resume
+
+[View Site](http://www.manuarora.me/type)
+
+## Building
+
+```bash
+git clone git@github.com:manuarora700/type-resume-v2.git
+cd type-resume-v2
+npm install
+npm run dev
+# Open localhost:4003/index-dev.html in your browser
+```
+
+## Building for Production
+
+```bash
+npm run build
+```
+
+Inspired from [strml.net](https://www.strml.net/) and [Jakeal Baugh](http://codepen.io/jakealbaugh/)
---
+++ webpack.config.js
@@ -0,0 +1,40 @@
+'use strict';
+var webpack = require('webpack');
+var path = require('path');
+
+// Builds bundle usable inside <script>.
+module.exports = {
+  context: __dirname,
+  mode: 'production',
+  entry: {
+    'app': './app.js'
+  },
+  output: {
+    path: path.join(__dirname, "/dist"),
+    filename: "[name].js",
+    libraryTarget: "umd",
+    library: "app",
+  },
+  devtool: 'source-map',
+  module: {
+    rules: [
+      {
+        test: /\.js?$/,
+        exclude: /node_modules/,
+        loader: 'babel-loader',
+        query: {
+          cacheDirectory: true,
+        }
+      }
+    ]
+  },
+  devServer: {
+    contentBase: __dirname,
+    publicPath: '/dist',
+    compress: true,
+    port: 4003,
+  },
+  optimization: {
+    minimize: true
+  },
+};
